6ef18fb76315f97cadc9fedba670dfc65bc23761
Jayesh Lilani <jayeshlilani@gmail.com>
2021-01-17 18:22:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,55 +58,33 @@ export default class App extends React.Component {
                                 <strong className="blue-text">J's PIZZA</strong>
                             </a>
 
-                            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                    data-target="#navbarSupportedContent"
-                                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                                    aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"/>
-                            </button>
-
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-                                <ul className="navbar-nav mr-auto">
-                                    <li className="nav-item active">
-                                        <button className="nav-link waves-effect"
-                                                onClick={() => this.switchPage("home")}
-                                                style={{background: 'none', border: 'none'}}>Home
-                                            <span className="sr-only">(current)</span>
-                                        </button>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link waves-effect"
-                                           target="_blank">About Us</a>
-                                    </li>
-                                </ul>
-
-                                <ul className="navbar-nav nav-flex-icons">
-                                    <li className="nav-item">
-                                        <button className="nav-link waves-effect"
-                                                onClick={() => this.switchPage("cart")}
-                                                style={{background: 'none', border: 'none'}}>
-                                            {sessionStorage.getItem('cart') == null ? console.log() :
-                                                <span
-                                                    className="badge red z-depth-1 mr-1"> {cart} </span>
-                                            }
-                                            <i className="fas fa-shopping-cart"/>
-                                            <span className="clearfix d-none d-sm-inline-block"> Cart </span>
-                                        </button>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="https://www.facebook.com/mdbootstrap" className="nav-link waves-effect"
-                                           target="_blank">
-                                            <i className="fab fa-facebook-f"/>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="https://twitter.com/MDBootstrap" className="nav-link waves-effect"
-                                           target="_blank">
-                                            <i className="fab fa-twitter"/>
-                                        </a>
-                                    </li>
-                                </ul>
+                            <div className="navbar-nav" id="navbarSupportedContent">
+                                {this.state.currentPage === 'cart' ?
+                                    <ul className="navbar-nav mr-auto">
+                                        <li className="nav-item active">
+                                            <button className="nav-link waves-effect"
+                                                    onClick={() => this.switchPage("home")}
+                                                    style={{background: 'none', border: 'none'}}>Home
+                                                <span className="sr-only">(current)</span>
+                                            </button>
+                                        </li>
+                                    </ul>
+                                    :
+                                    <ul className="navbar-nav nav-flex-icons">
+                                        <li className="nav-item">
+                                            <button className="nav-link waves-effect"
+                                                    onClick={() => this.switchPage("cart")}
+                                                    style={{background: 'none', border: 'none'}}>
+                                                {sessionStorage.getItem('cart') == null ? console.log() :
+                                                    <span
+                                                        className="badge red z-depth-1 mr-1"> {cart} </span>
+                                                }
+                                                <i className="fas fa-shopping-cart"/>
+                                                <span className="clearfix d-none d-sm-inline-block"> Cart </span>
+                                            </button>
+                                        </li>
+                                    </ul>
+                                }
                             </div>
                         </div>
                     </nav>
