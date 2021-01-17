599df3f3533ee76323343354e01e3d82cff23df2
Jayesh Lilani <jayeshlilani@gmail.com>
2021-01-17 17:33:10 +0530

    updated card import

--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import { Card,CardHeader,CardMedia,CardContent,CardActions,IconButton,Typography } from '@material-ui/core'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
